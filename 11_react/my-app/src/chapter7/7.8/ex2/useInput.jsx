2bcbf9f6b53a490f03733af5e3f7828a7f76c331
jenny.cha <chajg91@naver.com>
2023-01-26 14:22:57 +0900

    day 43 chapter 7
    ex2) Quiz 정답풀이
    ex3) 수업진도

--- 11_react/my-app/src/chapter7/7.8/ex2/useInput.jsx
+++ 11_react/my-app/src/chapter7/7.8/ex2/useInput.jsx
@@ -7,9 +7,19 @@ function useInput(initialValue) {
   const  handleChange = (e) => {
     setInputValue(e.target.value);
   };
+  // handleSubmit도 여기에 넣어서 함수를 리턴해준다
+  // const handleSubmit = () => {
+  //   alert(inputValue);
+  //   // setInputValue('');
+  // };
 
+  // input박스에서 reset기능만 구현
+  
+  const reset = () => {
+    setInputValue('');
+  }
   // 초기값과 handleChange 함수를 배열안에 넣어서 리턴해줌
-  return [inputValue, handleChange];
+  return [inputValue, handleChange, reset];
 
 }
 
