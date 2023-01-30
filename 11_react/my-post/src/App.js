029559ffb3e0094f4efb65921f00c239b027c64c
jenny.cha <chajg91@naver.com>
2023-01-30 18:29:38 +0900

    day 45 프로젝트 실습 예제
    map함수를 이용해서 post를 구성하고
    좋아요 수 늘려보기

--- 11_react/my-post/src/App.js
+++ 11_react/my-post/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import PostDetail from './components/PostDetail';
 
 
 function App() {
@@ -8,6 +9,13 @@ function App() {
   // 지금은 간단하게 배열안에 string만으로 
   // 나중엔 배열 안에 객체를 넣어서 할 예정
   const [posts, setPosts] = useState(['리액트 잘 쓰려면?','자바스크립트 핵심 문법','스타일링 가이드']);
+  
+  const [showPostDetail, setshowPostDetail] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  // 실제로는 객체안에 다 들어갈 좋아요 수
+  const [likeCount, setLikeCount] = useState([0, 0, 0]);
+  
+  
   return (
     <>
       {/* 상단 헤더 만들기 */}
@@ -38,17 +46,39 @@ function App() {
         </div>
 
       {/* Quiz: map()을 이용하여 포스트 반복 출력하기 */}
-      {posts.map((name, index) => {
+      {posts.map((post, index) => {
           return (
-            <>
-              <div className='list'>
-                <h4  key={index}>{name}</h4>
+            // 맵함수를 이용할 때는 제일 처음 감싸는 테그에다가 key값을 입력해 줘야 한다.
+              <div 
+                key={index} 
+                className='list'
+                onClick={() => {
+                  setshowPostDetail(true);
+                  setCurrentIndex(index);
+                }}
+                >
+                <h4>{post}</h4>
                 <p>2023년 1월 20일</p>
                 <p>by goni.kim</p>
+                <p>이것은 map함수로 구현된 포스트 입니다.</p>
+                <hr />
+
+                <span onClick={() => {
+                  // 배열을 조작하는 것이기 때문에 새로운 배열을 만들어 준다.
+                  const copyLikeCount = [...likeCount];
+                  copyLikeCount[index]++;
+                  setLikeCount(copyLikeCount);
+
+                }}
+                >❤️ {likeCount[index]}</span>
               </div>
-            </>
           );
         })}
+
+        {showPostDetail && <PostDetail posts={posts} setPosts={setPosts} currentIndex={currentIndex}/>}
+        {/* {showPostDetail ? <PostDetail /> : null} */}
+        
+
       </div>
     </>
     
