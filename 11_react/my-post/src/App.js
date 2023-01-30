89b00f305214cc432dbcb00bc37a77cca18b5922
jenny.cha <chajg91@naver.com>
2023-01-30 17:28:56 +0900

    day 45 프로젝트 실습 예제
    map함수를 이용해서 제목 넣기
    (배열만 구성)

--- 11_react/my-post/src/App.js
+++ 11_react/my-post/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  // 서버에서 데이터를 가져왔다고 가정 
+  // 지금은 간단하게 배열안에 string만으로 
+  // 나중엔 배열 안에 객체를 넣어서 할 예정
+  const [posts, setPosts] = useState(['리액트 잘 쓰려면?','자바스크립트 핵심 문법','스타일링 가이드']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* 상단 헤더 만들기 */}
+      <header className="header--dark">
+        <h4>Goni Post</h4>
+        <ul>
+          <li>트렌딩</li>
+          <li>최신</li>
+        </ul>
       </header>
-    </div>
+      
+      <div className='inner'>
+        {/* 포스트 목록 */}
+        <div className='list'>
+          <h4>{posts[0]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[1]}</h4>
+          <p>2023년 1월 27일</p>
+          <p>by jenny.cha</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[2]}</h4>
+          <p>2022년 11월 11일</p>
+          <p>by yg</p>
+        </div>
+
+      {/* Quiz: map()을 이용하여 포스트 반복 출력하기 */}
+      {posts.map((name, index) => {
+          return (
+            <>
+              <div className='list'>
+                <h4  key={index}>{name}</h4>
+                <p>2023년 1월 20일</p>
+                <p>by goni.kim</p>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+    
   );
 }
 
 export default App;
+/*  */
--- 11_react/my-post/src/index.js
+++ 11_react/my-post/src/index.js
@@ -6,9 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // 콘솔창에 두줄씩 나와서 지우는걸 추천
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
