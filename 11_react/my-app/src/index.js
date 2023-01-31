8d04fa220c75f8d942fc14cef7a02537b02c3329
jenny.cha <chajg91@naver.com>
2023-01-31 18:09:02 +0900

    day 46 13.1.3 children
    SignUpDialog 에서 Dialog 컴포넌트 호출 후 childeren요소 추가한 뒤
    Dialog 컴포넌트 안에서 렌더링 하는 부분에
    {props.children}을 적어주면 자동적으로 추가한 요소들이 렌더링됨!!

--- 11_react/my-app/src/index.js
+++ 11_react/my-app/src/index.js
@@ -45,6 +45,7 @@ import Calculatror from './chapter12/Calculator';
 import WelcomeDialog from './chapter13/13.1.1.1/WelcomeDialog';
 import SplitPaneSection from './chapter13/13.1.1.2/SplitPaneSection';
 import DialogContainer from './chapter13/13.1.2/DialogContainer';
+import SignUpDialog from './chapter13/13.1.3/SignUpDialog';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -200,7 +201,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render (
   // <WelcomeDialog/>
   // <SplitPaneSection />
-  <DialogContainer />
+  // <DialogContainer />
+  <SignUpDialog />
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
