77d16238a7ee752f0ba0ad6b9d090903bfb5df1a
jenny.cha <chajg91@naver.com>
2023-02-01 15:28:36 +0900

    day 47 Chapter14
    context를 받는 방법 useContext 라는 hook을 이용해서 가져온다.

--- 11_react/my-app/src/chapter14/MainContent.jsx
+++ 11_react/my-app/src/chapter14/MainContent.jsx
@@ -2,7 +2,11 @@ import { useContext } from "react";
 import ThemeContext from "./ThemeContext";
 
 function MainContent() {
-  const { theme, toggleTheme} = useContext(ThemeContext);
+  // Consumer대신에 간단하게 useContext Hook을 사용해서 
+  // 구조분해 할당으로 값을 가져온다.
+  // 함수 컴포넌트에서만 hook을 사용할 수 있다.
+  // 클래스 컴포넌트는 Consumer로 사용해야 함.
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (  
     <div
       style={{
