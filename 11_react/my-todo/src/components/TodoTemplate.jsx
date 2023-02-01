f19c93c7b16781d7f39e653471697d544fe4224d
jenny.cha <chajg91@naver.com>
2023-02-01 17:41:55 +0900

    day 47 Todo 프로젝트 실습
    TodoTemplate.jsx 작성

--- 11_react/my-todo/src/components/TodoTemplate.jsx
+++ 11_react/my-todo/src/components/TodoTemplate.jsx
@@ -6,15 +6,35 @@ margin: 0 auto;
 margin-top: 6rem;
 border-radius: 4px;
 overflow: hidden;
+
+/* 컴포넌트를 따로 만들어도 되고 아니면 Sass처럼 내부에 class로 만들어도 됨 */
+/* 자손을 의미 */
+.app-title {
+  background: #22b8cf;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+
+}
 `;
 
+// 컴포넌트를 따로 만드는 의미?
+
+
 // 화면을 가운데 정렬하고, 앱 타이틀을 보여주는 컴포넌트
-  // children으로 내부 자식 엘리먼트들을 props로 받아와서 렌더링
+// children으로 내부 자식 엘리먼트들을 props로 받아와서 렌더링
 function TodoTemplate(props) {
   const {children} = props;  
   return (  
     <TodoTemplateWrapper>
       <div className="app-title">일정관리</div>
+      <div className="content">{children}</div>
     </TodoTemplateWrapper>
 
   );
