55e4d6fcd305c862348afdcb9fcfd584854641c3
jenny.cha <chajg91@naver.com>
2023-02-01 15:40:51 +0900

    day 47 Chapter14
    context 이름 설정
    구글 개발자도구 Components에서 확인할 수 있는 명설정 이름.displayName = 이름;

--- 11_react/my-app/src/chapter14/ThemeContext.jsx
+++ 11_react/my-app/src/chapter14/ThemeContext.jsx
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 
 const ThemeContext = createContext('light');
-ThemeContext.displayName = 'ThemeContext'; // 리액트 개발자 도구에서 보일 컨텍스트 이름 설정
+console.log(ThemeContext);
+ThemeContext.displayName = 'ThemeContext'; // 리액트 개발자 도구에서 보일 컨텍스트 이름 설정 -> ThemeContext.Provider라고 나옴
+// displayName설정 안해주면 context.Provider로 헷갈려짐
 
 
 export default ThemeContext;
