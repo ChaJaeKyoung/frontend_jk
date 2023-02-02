a86fd33614c867b7262722f6198b8f6b7312db0a
jenny.cha <chajg91@naver.com>
2023-02-02 16:13:29 +0900

    day 48 Todo 프로젝트  id를 난수로 만들어보기
    react uuid 설치

--- 11_react/my-todo/src/components/TodoList.jsx
+++ 11_react/my-todo/src/components/TodoList.jsx
@@ -23,3 +23,13 @@ function TodoList({ todos, onRemove, onToggle }) { // 구조 분해 할당: 어�
 }
 
 export default TodoList;
+
+// HTML 웹 스토리지란?
+// 웹 스토리지를 사용하면 웹 앱이 사용자의 브라우저 내에 로컬로 데이터를 저장할 수 있음
+// 웹 스토리지는 도메인 당 사용 가능
+// 같은 도메인의 모든 페이지는 동일한 데이터를 저장하고 엑세스함
+
+// 웹 스토리지 객체
+// 웹 스토리지는 데이터를 저장하기 위한 두 가지 객체를 제공
+// window.localStorage - 만료 날짜 없이 데이터를 저장 
+// window.sessionStorage - 한 세션에 대한 데이터 저장(브라우저 탭을 닫으면 데이터가 손실됨(=초기화))
