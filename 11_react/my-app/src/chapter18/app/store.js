7cc30a93c91389dbe79922e05821e3159adf5ac1
jenny.cha <chajg91@naver.com>
2023-02-13 18:46:04 +0900

    day55 chapter18
    slice 에서 가져온reducer를 작명하는 부분 추가 설명

--- 11_react/my-app/src/chapter18/app/store.js
+++ 11_react/my-app/src/chapter18/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-//      ⬇️스스로 작명하는 부분
+//      ⬇️스스로 작명하는 부분 (export defalut 로 내보내는 것들을 작명으로 해서 가져올 수가 있는데, 관례적인 네이밍센스로 해야된다.)
 import counterReducer  from "../features/counter/counterSlice";
 // 1. Redux Store 만들기
 // 전역 상태를 보관하는 저장소
