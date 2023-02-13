674fcb2692b40a69eb7ae8deaed3f5eaf1db8114
jenny.cha <chajg91@naver.com>
2023-02-13 15:12:22 +0900

    day55 Reudx
    Counter 컴포넌트 만들기
    라인7, state 가져오는 부분 중요

--- 11_react/my-app/src/chapter18/features/counter/Counter.js
+++ 11_react/my-app/src/chapter18/features/counter/Counter.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
-// 5. 리액트 컴포넌트에서 Redux State와 Actions 사용하기
 
+// 5. 리액트 컴포넌트에서 Redux State와 Actions 사용하기
 function Counter(props) {
+  // Redux Store에 있는 state를 가져오는 함수
+  const count = useSelector((state) => state.counter.value); // state만 리턴하면 전역 state 전부 가져옴
+
+  // Redux Store에 요청을 보내주는 함수
+  const dispatch = useDispatch();
+
   return (
     <div>
-      
+      <div>
+        <button type='button'>
+          -1 감소
+        </button>
+        <span>{counter}</span>
+        <button type='button'>
+          +1 증가
+        </button>
+      </div>
     </div>
   );
 }
