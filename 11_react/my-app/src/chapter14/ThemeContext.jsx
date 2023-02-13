f89e419ba637759608b68d29909a5b72b762c4f2
jenny.cha <chajg91@naver.com>
2023-02-13 15:04:18 +0900

    console로그 감추기

--- 11_react/my-app/src/chapter14/ThemeContext.jsx
+++ 11_react/my-app/src/chapter14/ThemeContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 const ThemeContext = createContext('light');
-console.log(ThemeContext);
+// console.log(ThemeContext);
 ThemeContext.displayName = 'ThemeContext'; // 리액트 개발자 도구에서 보일 컨텍스트 이름 설정 -> ThemeContext.Provider라고 나옴
 // displayName설정 안해주면 context.Provider로 헷갈려짐
 
