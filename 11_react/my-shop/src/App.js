be9e9a01817584b8e21f3eaaa4adc0de58f9781f
jenny.cha <chajg91@naver.com>
2023-02-13 16:55:37 +0900

    day55 my-shop
    react-bootstrap 사용해서 버튼 나타내기

--- 11_react/my-shop/src/App.js
+++ 11_react/my-shop/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+
+// bootstrap: 레이아웃을 복사, 붙여넣기 식으로 편하게 개발 가능한 css, js라이브러리
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap CSS 추가
+import { Button } from "react-bootstrap";
+
+
+const GlobalStyle = createGlobalStyle`
+  /* 글로벌(공통) 스타일 */
+  body {
+    box-sizing: border-box;
+  }
+
+  #root {
+    text-align: center;
+  }
+
+  *{
+    box-sizing: inherit;
+  }
+
+  .cursor-pointer {
+    cursor: pointer;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <GlobalStyle />
+        {/* 부트스트랩 연습 */}
+        <Button variant="primary">Primary</Button>{' '}
+      </>
     </div>
   );
 }
