52f302cd55f0299fb0a18cd44c45318c8823d7c1
jenny.cha <chajg91@naver.com>
2023-03-13 17:28:03 +0900

    0313 typescript

--- 12_typescript/1_types.js
+++ 12_typescript/1_types.js
@@ -47,3 +47,109 @@ function greetUser(user) {
 greetUser({ name: 'Goni', age: 35 });
 // 객체의 모든 속성을 명시적으로 지정한 덕분에 코드를 보면 모든 사람이 
 // 해당 객체로 무엇을 할 수 있고 무엇을 할 수 없는지 쉽게 알 수 있음
+// Quiz
+// Q1. 여러분의 직업, 하루 공부 시간을 변수로 각각 저장해보세요.
+// 타입은 명시적으로 지정
+const job = 'student';
+const studyHour = 5;
+console.log(job, studyHour);
+// Q2. 여러분이 가장 좋아하는 곡과 가수 이름을 변수에 object 자료형으로 담아보세요.
+// 객체 안에는 노래 제목과 가수이름이 들어가면 됩니다.
+// 단, 제목과 가수는 문자만 들어올 수 있음
+const favoriteMusic = { song: 'waybackhome', singer: 'SHAUN' };
+console.log(favoriteMusic);
+let favorite = {
+    title: '소우주',
+    artist: 'BTS'
+};
+// Q3. 다음과 같이 생긴 자료의 타입 지정을 해보세요.
+let project = {
+    member: ['Kim', 'Park'],
+    days: 30,
+    started: true // started 속성은 true/false만 들어올 수 있음
+};
+console.log(project);
+// 6. 튜플(tuple)
+// 튜플을 사용하면 배열의 요소에 자료형을 정의할 수 있다.
+let myTuple = ['hi', 5, 'hello'];
+// TS는 튜플에 정의된 인덱스의 자료형은 알고 있지만, 배열에 새롭게 추가되는 요소의 자료형을 알 수는 없다.
+myTuple.push(3);
+myTuple.push('aaa');
+console.log(myTuple);
+// 7. enum(enumarated type)
+// 열거형 enum을 이용하면 숫자 집합에 이름을 부여할 수 있다.
+var Status;
+(function (Status) {
+    Status[Status["deleted"] = 0] = "deleted";
+    Status[Status["pending"] = 1] = "pending";
+    Status[Status["active"] = 2] = "active"; //2
+})(Status || (Status = {}));
+const blogPostStatus = Status.active;
+console.log(blogPostStatus);
+// 열거형 내부의 값은 0부터 시작
+// 블로그 게시물의 상태를 표현할 때 2가 아닌 active라고 하는 것이 가독성이 좋음
+// 다음과 같이 시작값을 지정하여 열거형의 숫자를 원하는 값부터 할당할 수도 있음
+var Week;
+(function (Week) {
+    Week[Week["MON"] = 1] = "MON";
+    Week[Week["TUE"] = 2] = "TUE";
+    Week[Week["WED"] = 3] = "WED";
+    Week[Week["THU"] = 4] = "THU";
+    Week[Week["FRI"] = 8] = "FRI";
+    Week[Week["SAT"] = 9] = "SAT";
+    Week[Week["SUN"] = 10] = "SUN"; //10
+})(Week || (Week = {}));
+const todayWeek = Week.SUN;
+console.log(todayWeek);
+// 반대로 숫자를 이용하여 열거형 값에 접근 가능
+console.log(Week[4]);
+// 8. any
+// 어느, 어떤, 아무것이든 하나
+// 무엇이든 될 수 있음을 의미(모든 자료형이 다 들어올 수 있음)
+let anyValue = 123;
+anyValue = 'aaa';
+anyValue = true;
+anyValue = [];
+// 편하지만 타입스크립트를 쓰는 의미가 없어짐.. (일반 JS 변수가 됨)
+// 자바스크립트에서 기존 코드들을 활용하면서 부분적 타입스크립트 적용할 때 사용
+// 9. void
+// 빈 공간, 하나도[전혀] 없는
+// 자료형이 없음을 의미
+// 함수의 리턴 타입으로 자주 사용됨
+function storeValueInDatabase(objectToStore) {
+    // 데이터베이스에 값을 저장
+}
+// 이 함수는 객체를 받아서 데이터베이스에 저장하지만 아무것도 반환하지 않기때문에 반환값(리턴값)을 void로 지정
+// 10. null과 undefined
+// null 또는 undefined 자료형의 변수를 만드는 것은 유용하지 않음
+// => null 및 undefined 값만 할당 할 수 있기 때문
+// 주로 유니언 자료형 만들 때 사용(예: string | undefined)
+// 11. never
+// never는 절대 발생하지 않는 값
+// 예를 들어 반환을 아예 하지 않거나 항상 오류를 발생시키는 함수에 사용할 수 있음
+function throwError(error) {
+    throw new Error(error);
+}
+// 12. union type(타입을 2개 이상 합친 새로운 타입)
+let unionValue = 'hi'; // 문자 또는 숫자가 들어올 수 있음
+console.log(unionValue);
+unionValue = 123;
+console.log(unionValue);
+// 문자 또는 숫자가 담길 수 있는 배열 타입 지정은?
+let unionArray = [1, '나비', 3, 4, '표범'];
+// let unionArray2: string | number[] = '나비';
+// let unionArray3: string[] | number[] = [1,2,3];
+console.log(unionArray);
+// 문자 또는 숫자가 가능한 객체 타입 지정은?
+let unionObject = { name: 'Kim' };
+console.log(unionObject);
+// (참고)
+// 변수를 선언만 하면 자동으로 any 타입
+// 확인법: 변수에 마우스 커서 올려두면 뜸
+let value;
+value = 123;
+value = '나무';
+// 값을 할당하면 그 때 타입이 정해짐(동적 타입)
+// 선언과 동시에 초기화 시 타입 지정은 자동으로 됨
+let value2 = 123; // 타입 지정을 별도로 하지 않아도 자동으로 number로 됨
+// value2= '토끼'; // Type Error
